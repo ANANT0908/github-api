f9208c7fd2de340540b8df980841164868a5132e
ANANT0908 <87381952+ANANT0908@users.noreply.github.com>
2021-08-29 23:54:52 +0530

    problem solved

--- src/component/useEffect/useEffectApi.js
+++ src/component/useEffect/useEffectApi.js
@@ -28,8 +28,8 @@ const UseEffectAPI = () =>{
                                         <div className="d-flex align-items-center">
                                             <div className="image"> <img src={currEl.avatar_url} className="rounded" width="155"/></div>
                                             <div className="ml-3 w-100">
-                                                <h4 className="mb-0 mt-0 textLeft">{currEl.login}</h4><span className="textLeft">Web Developer</span>
-                                                <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
+                                                <h4 className="mb-4 textLeft">{currEl.login}</h4>
+                                                <div className="p-2 mt-2 d-flex justify-content-between  text-white stats">
                                                     <div className="d-flex flex-column"> <span className="articles">ID</span> <span className="number1">{currEl.id}</span></div>
                                                     <div className="d-flex flex-column"> <span className="followers">Node ID</span> <span className="number2">{currEl.node_id}</span></div>
                                                     <div className="d-flex flex-column"> <span className="rating">Type</span> <span className="number3">{currEl.type}</span></div>
